af5e42e9efdbbcc5f6444114b2dcd8a7f684430a
EhsanAiDev <ehsan.aidev@gmail.com>
2025-06-11 09:42:48 +0330

    Initial clean commit

---
+++ promodo_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PromodoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'promodo_app'
---
+++ promodo_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pro_home(r):
+    return render(r,"promodo_app/pro_home.html")

a3f9b707b1e8a8d93b6cda4c36595e31749a2ac0
EhsanAiDev <ehsan.aidev@gmail.com>
2025-06-13 09:04:49 +0330

    delete hole project :)

--- promodo_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PromodoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'promodo_app'
--- promodo_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def pro_home(r):
-    return render(r,"promodo_app/pro_home.html")
